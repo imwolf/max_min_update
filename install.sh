0a309ebe7f753aa0c0d80f7325b2ff8858c71479
imwolf <neverjay@gmail.com>
2013-10-26 20:29:11 +0800

    a shell script to install image to node

---
+++ install.sh
@@ -0,0 +1,6 @@
+sudo chmod a+rw /dev/ttyUSB*
+make telosb
+make telosb reinstall.0 bsl,/dev/ttyUSB0 & make telosb reinstall.1 bsl,/dev/ttyUSB1 & make telosb reinstall.2 bsl,/dev/ttyUSB2 & make telosb reinstall.3 bsl,/dev/ttyUSB3 & make telosb reinstall.4 bsl,/dev/ttyUSB4 & make telosb reinstall.5 bsl,/dev/ttyUSB5 & make telosb reinstall.6 bsl,/dev/ttyUSB6
+#make telosb reinstall.0 bsl,/dev/ttyUSB3 & make telosb reinstall.1 bsl,/dev/ttyUSB21 & make telosb reinstall.2 bsl,/dev/ttyUSB4 & make telosb reinstall.3 bsl,/dev/ttyUSB11 & make telosb reinstall.4 bsl,/dev/ttyUSB19 & make telosb reinstall.5 bsl,/dev/ttyUSB5 & make telosb reinstall.6 bsl,/dev/ttyUSB20
+#make telosb reinstall.0 bsl,/dev/ttyUSB3 & make telosb reinstall.1 bsl,/dev/ttyUSB21 & make telosb reinstall.2 bsl,/dev/ttyUSB11 & make telosb reinstall.3 bsl,/dev/ttyUSB15 & make telosb reinstall.4 bsl,/dev/ttyUSB19 & make telosb reinstall.5 bsl,/dev/ttyUSB5 & make telosb reinstall.6 bsl,/dev/ttyUSB20
+
